fcd0f660b2d57962e66e935967b9b5cd93032515
ferreira0610 <ferreirahtml22@gmail.com>
2024-03-05 09:58:40 -0300

    teste app

--- app.py
+++ app.py
@@ -3,25 +3,29 @@ import openpyxl
 import random
 
 tabela_nomes = pd.read_excel('planilha_nomes (1).xlsx')
-# qntd_linhas_tabela_nomes = len(tabela_nomes)
+qntd_linhas_tabela_nomes = len(tabela_nomes)
 # print(tabela_nomes)
 # print(qntd_linhas_tabela_nomes)
-lista_nomes = []
-for linha in enumerate(tabela_nomes):
-    _, nome, *_ = linha
-    print(nome)
-    lista_nomes.append(nome)
-print(lista_nomes)
+
+# lista_nomes = []
+# for linha in enumerate(tabela_nomes):
+#     _, nome, *_ = linha
+#     print(nome)
+#     lista_nomes.append(nome)
+# print(lista_nomes)
+
+
 tabela_sobrenomes = pd.read_excel('planilha_sobrenomes (1).xlsx')
-# qntd_linhas_tabela_sobrenomes = len(tabela_sobrenomes)
+qntd_linhas_tabela_sobrenomes = len(tabela_sobrenomes)
 # print(tabela_sobrenomes)
 # print(qntd_linhas_tabela_sobrenomes)
-lista_sobrenomes = []
-for linha in enumerate(tabela_sobrenomes):
-    _, sobrenome, *_ = linha
-    print(sobrenome)
-    lista_sobrenomes.append(sobrenome)
-print(lista_sobrenomes)
+
+# lista_sobrenomes = []
+# for linha in enumerate(tabela_sobrenomes):
+#     _, sobrenome, *_ = linha
+#     print(sobrenome)
+#     lista_sobrenomes.append(sobrenome)
+# print(lista_sobrenomes)
 
 
 
@@ -40,21 +44,21 @@ while True:
 
 for i in range(qntd_nomes):
 
-    # linha_random_nome = random.randint(1, qntd_linhas_tabela_nomes-1)
-    # nome = tabela_nomes.iloc[linha_random_nome, 0]
-    nome = random.choice(tabela_nomes)
+    linha_random_nome = random.randint(1, qntd_linhas_tabela_nomes-1)
+    nome = tabela_nomes.iloc[linha_random_nome, 0]
+    # nome = random.choice(tabela_nomes)
     # print(nome)
     
-    # linha_random_sobrenome_1 = random.randint(0, qntd_linhas_tabela_sobrenomes-1)
-    # sobrenome_1 = tabela_sobrenomes.iloc[linha_random_sobrenome_1, 0]
-    sobrenome_1 = random.choice(tabela_sobrenomes)
+    linha_random_sobrenome_1 = random.randint(0, qntd_linhas_tabela_sobrenomes-1)
+    sobrenome_1 = tabela_sobrenomes.iloc[linha_random_sobrenome_1, 0]
+    # sobrenome_1 = random.choice(tabela_sobrenomes)
 
     # print(sobrenome_1)
 
     while True:
-        # linha_random_sobrenome_2 = random.randint(1, qntd_linhas_tabela_sobrenomes-1)
-        # sobrenome_2 = tabela_sobrenomes.iloc[linha_random_sobrenome_2, 0]
-        sobrenome_2 = random.choice(tabela_sobrenomes)
+        linha_random_sobrenome_2 = random.randint(1, qntd_linhas_tabela_sobrenomes-1)
+        sobrenome_2 = tabela_sobrenomes.iloc[linha_random_sobrenome_2, 0]
+        # sobrenome_2 = random.choice(tabela_sobrenomes)
 
         # print(sobrenome_2)
         
